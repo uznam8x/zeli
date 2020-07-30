f629f7c5d6e11d4c52c1fa55dfe7c2f315b389fe
jinam.yu <jinam.yu@myrealtrip.com>
2020-07-30 19:32:21 +0900

    test

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var pipe = require('./lib/pipe');
 var parse = require('./lib/parse');
 
 function splitNewLine(text = '') {
-  return text.split('\n');
+  return text.split('---').map(function (value) {
+    return value.replace(/\n/g, ' ');
+  });
 }
 function stringTrim(arr = []) {
   return arr.map(function (value) {
--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 var zeli = require('./index.js');
-zeli(`
-    [rect = {width: '100', height: '60', style:"fill:red;"},rect] --> group --> svg
+var canvas = zeli(`
+    [rect = {width: '100'}, rect]
+    --> group
+    --> svg
 `);
